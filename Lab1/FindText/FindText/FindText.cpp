bb2c64defbc119a3d1352796832a0b6839dfa538
x4la1 <lebedevdenis805@gmail.com>
2025-02-09 23:22:10 +0300

    add FindText

---
+++ Lab1/FindText/FindText/FindText.cpp
@@ -0,0 +1,59 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Invalid arguments count\n";
+        cout << "Usage: FindText.exe <file name> <text for search>\n";
+        return 1;
+    }
+
+    ifstream input;
+    input.open(argv[1]);
+    if (!input.is_open())
+    {
+        cout << "Failed to open '" << argv[1] << "'\n";
+        return 1;
+    }
+
+    string searchText = argv[2];
+    if (searchText == "")
+    {
+        cout << "Text for search must not be empty\n";
+        return 1;
+    }
+
+
+    vector<int> lineNumbers = {};
+    std::string line;
+    int lineNumber = 0;
+    while (getline(input, line))
+    {
+        lineNumber++;
+        if (line.find(searchText) != string::npos)
+        {
+            lineNumbers.push_back(lineNumber);
+        }
+    }
+
+    if (lineNumbers.empty())
+    {
+        cout << "Text not found\n";
+    }
+    else
+    {
+        for (int item : lineNumbers)
+        {
+            cout << item << "\n";
+        }
+    }
+
+
+    return 0;
+}
