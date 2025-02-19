f6add5d84d0addfb4a07bee010006cb8e9e9778c
x4la1 <lebedevdenis805@gmail.com>
2025-02-20 00:13:22 +0300

    fix FindText

--- Lab1/FindText/FindText/FindText.cpp
+++ Lab1/FindText/FindText/FindText.cpp
@@ -1,70 +1,101 @@
-﻿#include <fstream>
+﻿#include <cstdint>
+#include <fstream>
 #include <iostream>
+#include <optional>
 #include <string>
 #include <vector>
 
-bool IsValidParameters(int argc, char* argv[])
+struct Arguments
 {
-    if (argc != 3)
-    {
-        std::cout << "Invalid arguments count\n";
-        std::cout << "Usage: FindText.exe <file name> <text for search>\n";
-        return false;
-    }
+	std::string filename;
+	std::string searchText;
+};
 
-    std::string searchText = argv[2];
-    if (searchText == "")
-    {
-        std::cout << "Text for search must not be empty\n";
-        return false;
-    }
+std::optional<Arguments> ParseArguments(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		std::cout << "Invalid arguments count\n";
+		std::cout << "Usage: FindText.exe <file name> <text for search>\n";
+		return std::nullopt;
+	}
 
-    return true;
+	std::string searchText(argv[2]);
+	if (searchText == "")
+	{
+		std::cout << "Text for search must not be empty\n";
+		return std::nullopt;
+	}
+
+	Arguments args{};
+	args.filename = std::string{argv[1]};
+	args.searchText = searchText;
+
+	return args;
 }
 
-void FindText(std::ifstream &input, std::string searchText)
+void FindText(std::ifstream& input, const std::string& searchText)
 {
-    std::vector<int> lineNumbers;
-    std::string line;
-    int lineCounter = 0; //name
-    while (getline(input, line))
-    {
-        lineCounter++;
-        if (line.find(searchText) != std::string::npos)
-        {
-            lineNumbers.push_back(lineCounter);
-        }
-    }
+	std::vector<uint32_t> lineNumbers{};
+	std::string line{};
+	uint32_t lineCounter = 0;
+	while (getline(input, line))
+	{
+		lineCounter++;
+		if (line.find(searchText) != std::string::npos)
+		{
+			lineNumbers.push_back(lineCounter);
+		}
+	}
 
-    if (lineNumbers.empty())
-    {
-        std::cout << "Text not found\n";
-    }
-    else
-    {
-        for (int item : lineNumbers)
-        {
-            std::cout << item << "\n";
-        }
-    }
+	if (lineNumbers.empty())
+	{
+		std::cout << "Text not found\n";
+	}
+	else
+	{
+		for (auto item : lineNumbers)
+		{
+			std::cout << item << "\n";
+		}
+	}
 }
+
+bool IsFileReadble(std::ifstream& input)
+{
+	if (input.peek() == EOF)
+	{
+		std::cout << "File can't be read\n";
+		return false;
+	}
+	return true;
+}
+
+//ParseArguments возвращает объект
+//пустой файл 
+//права доступа
 
 int main(int argc, char* argv[])
 {
-    //функция возвращает структуру аргументов
-    //проверка пустой строки в findText
-    //права доступа
-    if (!IsValidParameters(argc, argv)) return 1;
+	auto args = ParseArguments(argc, argv);
+	if (args == std::nullopt)
+	{
+		return 1;
+	}
 
-    std::ifstream input;
-    input.open(argv[1]);
-    if (!input.is_open())
-    {
-        std::cout << "Failed to open '" << argv[1] << "'\n";
-        return 0;
-    }
-    
-    FindText(input, argv[2]);
+	std::ifstream input(args->filename);
+	if (!input.is_open())
+	{
+		std::cout << "Failed to open '" << argv[1] << "'\n";
+		return 1;
+	}
+	
+	if (!IsFileReadble(input))
+	{
+		return 1;
+	}
+	
+	FindText(input, args->searchText);
 
-    return 0;
+	return 0;
 }
--- Lab1/FindText/FindText/Tests.bat
+++ Lab1/FindText/FindText/Tests.bat
@@ -2,33 +2,50 @@ SET MyProgram="%~1"
 SET Out="%TEMP%\out.txt"
 
 
-%MyProgram% input1.txt privet poka > %Out% || goto err
-fc %Out% output1.txt > nul|| goto err
+%MyProgram% input1.txt privet poka > %Out% 
+IF NOT ERRORLEVEL 1 goto err
+fc %Out% output1.txt || goto err
 echo 1 test passed
 
-%MyProgram% input2.txt > %Out% || goto err
-fc %Out% output2.txt > nul|| goto err
+%MyProgram% input2.txt > %Out%
+IF NOT ERRORLEVEL 1 goto err
+fc %Out% output2.txt || goto err
 echo 2 test passed
 
-%MyProgram% input3.txt "" > %Out% || goto err
-fc %Out% output3.txt > nul|| goto err
+%MyProgram% input3.txt "" > %Out% 
+IF NOT ERRORLEVEL 1 goto err
+fc %Out% output3.txt || goto err
 echo 3 test passed
 
-%MyProgram% unknown.txt privet > %Out% || goto err
-fc %Out% output4.txt > nul|| goto err
+%MyProgram% unknown.txt privet > %Out% 
+IF NOT ERRORLEVEL 1 goto err
+fc %Out% output4.txt || goto err
 echo 4 test passed
 
-%MyProgram% input5.txt privet > %Out% || goto err
-fc %Out% output5.txt > nul|| goto err
+%MyProgram% input5.txt privet > %Out%
+IF ERRORLEVEL 1 goto err
+fc %Out% output5.txt || goto err
 echo 5 test passed
 
-%MyProgram% input6.txt privet > %Out% || goto err
-fc %Out% output6.txt > nul|| goto err
+%MyProgram% input6.txt privet > %Out% 
+IF ERRORLEVEL 1 goto err
+fc %Out% output6.txt || goto err
 echo 6 test passed
 
-%MyProgram% input7.txt privet > %Out% || goto err
-fc %Out% output7.txt > nul|| goto err
+%MyProgram% input7.txt privet > %Out%
+IF ERRORLEVEL 1 goto err
+fc %Out% output7.txt || goto err
 echo 7 test passed
+
+%MyProgram% input8.txt privet > %Out%
+IF NOT ERRORLEVEL 1 goto err
+fc %Out% output8.txt || goto err
+echo 8 test passed
+
+%MyProgram% input9.txt privet > %Out%
+IF NOT ERRORLEVEL 1 goto err
+fc %Out% output9.txt || goto err
+echo 9 test passed
 
 
 echo all tests passed
