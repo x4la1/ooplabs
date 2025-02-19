860d7a8677cb0cf5143052b44db2a21db45e795b
x4la1 <lebedevdenis805@gmail.com>
2025-02-20 00:41:30 +0300

    fix MagicNumbers

--- Lab1/MagicNumbers/MagicNumbers/MagicNumbers.cpp
+++ Lab1/MagicNumbers/MagicNumbers/MagicNumbers.cpp
@@ -1,48 +1,56 @@
-﻿#include <iostream>
-#include <cstdint>
+﻿#include <cstdint>
+#include <iostream>
+#include <optional>
 #include <string>
+#include <cstdint>
 
-bool IsValidParams(int argc, char* argv[])
+bool IsValidArguments(int argc, char* argv[])
 {
-    if (argc == 2 || argc > 3)
-    {
-        std::cout << "Usage: MagicNumbers.exe -find <number> OR MagicNumbers.exe\n";
-        return false;
-    }
-    else if (argc == 3)
-    {
-        std::string firstParameter = argv[1];
-        if (firstParameter != "-find")
-        {
-            std::cout << "Usage: MagicNumbers.exe -find <number> OR MagicNumbers.exe\n";
-            return false;
-        }
-        else
-        {
-            std::cout << "There is no magic numbers in this range\n"; //вынести
-            return false;
-        }
-    }
+	if (argc == 2 || argc > 3)
+	{
+		std::cout << "Usage: MagicNumbers.exe -find <number> OR MagicNumbers.exe\n";
+		return false;
+	}
 
-    return true;
+	if (argc == 3)
+	{
+		std::string firstParameter = argv[1];
+		if (firstParameter != "-find")
+		{
+			std::cout << "Usage: MagicNumbers.exe -find <number> OR MagicNumbers.exe\n";
+			return false;
+		}
+	}
+	return true;
 }
 
-//вынести isMagicNumbers
-
+//вынести isMagicNumber
+//-find logica
 int main(int argc, char* argv[])
 {
-    if (!IsValidParams(argc, argv)) return 0;
+	if (argc != 1)
+	{
+		if (IsValidArguments(argc, argv))
+		{
+			std::cout << "There is no magic numbers in this range\n";
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
 
-    uint64_t number = 0;
-    if (std::cin >> number)
-    {
-        std::cout << "Non-magic\n";
-    }
-    else
-    {
-        std::cout << "Error\n";
-    }
+	uint64_t number = 0;
+	if (std::cin >> number)
+	{
+		std::cout << "Non-magic\n";
+	}
+	else
+	{
+		std::cout << "Error\n";
+		return 1;
+	}
 
-    return 0;
+	return 0;
 }
-
--- Lab1/MagicNumbers/MagicNumbers/Tests.bat
+++ Lab1/MagicNumbers/MagicNumbers/Tests.bat
@@ -2,27 +2,33 @@
 SET MyProgram="%~1"
 SET Out="%TEMP%\out.txt"
 
-%MyProgram% "-find" > %Out% || goto err
+%MyProgram% "-find" > %Out% 
+IF NOT ERRORLEVEL 1 goto err
 fc %Out% output1.txt || goto err
 echo 1 test passed
 
-%MyProgram% "-find" 1 2 > %Out% || goto err
+%MyProgram% "-find" 1 2 > %Out% 
+IF NOT ERRORLEVEL 1 goto err
 fc %Out% output2.txt || goto err
 echo 2 test passed
 
-%MyProgram% 1 2 > %Out% || goto err
+%MyProgram% 1 2 > %Out% 
+IF NOT ERRORLEVEL 1 goto err
 fc %Out% output3.txt || goto err
 echo 3 test passed
 
-%MyProgram% < input4.txt > %Out% || goto err
+%MyProgram% < input4.txt > %Out% 
+IF NOT ERRORLEVEL 1 goto err
 fc %Out% output4.txt || goto err
 echo 4 test passed
 
-%MyProgram% < input5.txt > %Out% || goto err
+%MyProgram% < input5.txt > %Out% 
+IF ERRORLEVEL 1 goto err
 fc %Out% output5.txt || goto err
 echo 5 test passed
 
-%MyProgram% "-find" 1 > %Out% || goto err
+%MyProgram% "-find" 1 > %Out% 
+IF ERRORLEVEL 1 goto err
 fc %Out% output6.txt || goto err
 echo 6 test passed
 
