888d5a34178e38746d955257d2bea4e653ebb473
x4la1 <lebedevdenis805@gmail.com>
2025-05-21 17:24:31 +0300

    fix main httpurl

--- Lab6/HttpUrl/HttpUrl/main.cpp
+++ Lab6/HttpUrl/HttpUrl/main.cpp
@@ -7,14 +7,20 @@ int main()
 	std::string line;
 	while (std::getline(std::cin, line))
 	{
-		HttpUrl url(line);
-		std::cout << "Protocol: " << ProtocolToString(url.GetProtocol()) << "\n"
-				  << "Domain: " << url.GetDomain() << "\n"
-			      << "Port: " << url.GetPort() << "\n"
-			      << "Document: " << url.GetDocument() << "\n"
-			      << "Url: " << url.GetURL() << "\n";
+		try
+		{
+			HttpUrl url(line);
+			std::cout << "Protocol: " << ProtocolToString(url.GetProtocol()) << "\n"
+				<< "Domain: " << url.GetDomain() << "\n"
+				<< "Port: " << url.GetPort() << "\n"
+				<< "Document: " << url.GetDocument() << "\n"
+				<< "Url: " << url.GetURL() << "\n";
+		}
+		catch(std::exception& e)
+		{
+			std::cout << e.what();
+		}
 	}
-	std::cout << "Hello World!\n";
 }
 
 
